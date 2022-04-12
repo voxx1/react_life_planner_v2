d3dd9a35948a8e150f86eb5855d54165027a0127
voxx1 <61800692+voxx1@users.noreply.github.com>
2022-04-13 00:29:01 +0200

    fixing dummy data

--- src/components/dummy-data/dummy_goals.js
+++ src/components/dummy-data/dummy_goals.js
@@ -1,17 +1,17 @@
 const DUMMY_GOALS = [{
     id: 1,
-    title: "Buy flowers for mom",
-    description: "siemka mamo"
+    title: "Get flowers for my girlfriend!",
+    description: "I have to give her some present"
 },
 {
     id: 2,
     title: "Get some food for breakfast",
-    description: "siemka mamo"
+    description: "I would like to eat some eggs and bread"
 },
 {
     id: 3,
-    title: "Buy some milk",
-    description: "siemka mamo"
+    title: "Call grandma",
+    description: "It's been a long time since I talked to my grandmother, it's time to catch up!"
 }]
 
 export default DUMMY_GOALS
